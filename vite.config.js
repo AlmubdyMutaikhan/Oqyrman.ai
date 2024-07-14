7dfdad9ed0e6ceec19fd6b350e04fa92b1fe5c45
AlmubdyMutaikhan <mutaikhan.almubdy@gmail.com>
2024-07-14 18:11:24 +0500

    work

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { sassPlugin } from 'esbuild-sass-plugin';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sassPlugin()],
-  build: {
-    outDir: 'build', // make sure this matches what Vercel is expecting
-    rollupOptions: {
-      // further configuration if needed
-    }
-  }
 })
