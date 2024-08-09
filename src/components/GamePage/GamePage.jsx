b6c14c2b8c505e25277d9bc655cd0755ae3ec9a2
AlmubdyMutaikhan <mutaikhan.almubdy@gmail.com>
2024-08-09 10:46:11 +0500

    f

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './GamePage.scss'; // Create a custom CSS file for styling
+import './GamePage.scss'; // Import the custom CSS file
 import Modal from '../Modal/Modal';
 
 const GamePage = () => {
@@ -7,7 +7,6 @@ const GamePage = () => {
   const [isLandscape, setIsLandscape] = useState(window.innerWidth > window.innerHeight);
 
   useEffect(() => {
-    // Function to check orientation
     const checkOrientation = () => {
       if (window.innerWidth > window.innerHeight) {
         setIsLandscape(true);
@@ -18,25 +17,20 @@ const GamePage = () => {
       }
     };
 
-    // Check orientation on load
     checkOrientation();
 
-    // Add event listener to detect orientation changes
     window.addEventListener('resize', checkOrientation);
 
     return () => {
-      // Remove event listener on cleanup
       window.removeEventListener('resize', checkOrientation);
     };
   }, []);
 
   useEffect(() => {
-    // Ensure the iframe takes the full screen size
     document.documentElement.style.overflow = 'hidden';
     document.body.style.overflow = 'hidden';
 
     return () => {
-      // Reset the overflow styles when the component is unmounted
       document.documentElement.style.overflow = '';
       document.body.style.overflow = '';
     };
@@ -46,7 +40,7 @@ const GamePage = () => {
     <div className="game-page">
       <Modal
         isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(true)} // Keep the modal open if the orientation changes to portrait
+        onClose={() => setIsModalOpen(true)}
         translation={'Маңызды !'}
         description={'Ойыннан керемет әсер алу үшін, телефонды көлденең ұстаңыз'}
         isGame
